a54a9aafc59f77519a5494711e97875625a8b4d0
ViniF <viniciosfaria10@gmail.com>
2025-11-03 10:13:58 -0300

    commit-0.3
    Transição suave entre paginas

--- Inicio/script.js
+++ Inicio/script.js
@@ -1,12 +1,5 @@
- const meuBotao1 = document.querySelector('#bt_rupestre');
+const meuBotao1 = document.querySelector('#bt_rupestre');
 const meuBotao2 = document.querySelector('#bt_egipcia');
-
-function aplicarTransicao(url) {
-    document.body.classList.add('fade-out');
-    setTimeout(() => {
-    window.location.href = url;
-    }, 100);
-}
 
 meuBotao1.addEventListener('click', (event) => {
     event.preventDefault();
@@ -16,4 +9,15 @@ meuBotao1.addEventListener('click', (event) => {
 meuBotao2.addEventListener('click', (event) => {
     event.preventDefault();
     aplicarTransicao("http://127.0.0.1:5500/Egipcia/egipcia.html");
+});
+
+
+function aplicarTransicao(destino) {
+    document.body.classList.add('fade-out');
+    setTimeout(() => {
+        window.location.href = destino;
+    }, 300);
+}
+window.addEventListener('load', () => {
+    document.body.classList.remove('fade-out');
 });

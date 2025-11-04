c45338d7788b7f2b99b784b1ca2fd03c35dcfad9
ViniF <viniciosfaria10@gmail.com>
2025-11-03 22:23:19 -0300

    commit-0.5
    Primeira transição da pagina feita.

--- Inicio/script.js
+++ Inicio/script.js
@@ -1,23 +1,42 @@
-const meuBotao1 = document.querySelector('#bt_rupestre');
-const meuBotao2 = document.querySelector('#bt_egipcia');
+const btRupestre = document.querySelector('#bt_rupestre');
+const btEgipcia = document.querySelector('#bt_egipcia');
 
-meuBotao1.addEventListener('click', (event) => {
-    event.preventDefault();
-    aplicarTransicao("http://127.0.0.1:5500/Rupestre/rupestre.html");
-});
+function criarOverlay(imagem, lado, destino) {
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    overlay.style.backgroundImage = `url('${imagem}')`;
 
-meuBotao2.addEventListener('click', (event) => {
-    event.preventDefault();
-    aplicarTransicao("http://127.0.0.1:5500/Egipcia/egipcia.html");
-});
+    if(lado === 'esquerda') {
+        overlay.style.left = '0';
+        overlay.style.transformOrigin = 'left'; // origem esquerda
+    } else {
+        overlay.style.right = '0';
+        overlay.style.transformOrigin = 'right'; // origem direita
+    }
 
+    // Adiciona no DOM
+    document.body.appendChild(overlay);
 
-function aplicarTransicao(destino) {
-    document.body.classList.add('fade-out');
+    // Bloqueia botões
+    document.querySelectorAll('.botao').forEach(b => b.style.pointerEvents = 'none');
+
+    // Expande overlay (mesma transição para ambos)
+    setTimeout(() => {
+        overlay.classList.add('expandir');
+    }, 50);
+
+    // Redireciona após animação
     setTimeout(() => {
         window.location.href = destino;
-    }, 300);
+    }, 1500);
 }
-window.addEventListener('load', () => {
-    document.body.classList.remove('fade-out');
+
+btRupestre.addEventListener('click', e => {
+    e.preventDefault();
+    criarOverlay('../img/rupestrefundo.png', 'esquerda', 'http://127.0.0.1:5500/Rupestre/rupestre.html');
+});
+
+btEgipcia.addEventListener('click', e => {
+    e.preventDefault();
+    criarOverlay('../img/egipcia_fundo.png', 'direita', 'http://127.0.0.1:5500/Egipcia/egipcia.html');
 });

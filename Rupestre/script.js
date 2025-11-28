50b13418592b35016dce069429cf4f3d7b7d2623
ViniF <viniciosfaria10@gmail.com>
2025-11-27 22:19:55 -0300

    commit - 1.5
    Imagem de fundo mudada e colocado um botão da maneira correta.

--- Rupestre/script.js
+++ Rupestre/script.js
@@ -11,3 +11,27 @@ window.addEventListener("load", () => {
         el.classList.add("ativo");
     });
 })
+
+const abrir = document.getElementById("abrirjanela");
+const janela = document.getElementById("janela");
+const fechar = document.getElementById("fechar");
+
+abrir.onclick = () => {
+    janela.style.display = "block";
+};
+
+fechar.onclick = () => {
+    janela.style.display = "none";
+};
+
+const abri2 = document.getElementById("abrirjanela2");
+const janela2 = document.getElementById("janela2");
+const fechar2 = document.getElementById("fechar2");
+
+abrir2.onclick = () => {
+    janela2.style.display = "block";
+};
+
+fecha2.onclick = () => {
+    janela2.style.display = "none";
+};
